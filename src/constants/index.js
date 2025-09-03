44076ecad9951fc35224bb86bc5df752f56acc5b
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-09-03 19:36:05 +0530

    did some changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,7 +116,7 @@ const experiences = [
     company_name: "Printedge India",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "June 2025 - August 2025",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
